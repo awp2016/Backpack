64f864a52b4bb1705f568a78924b546615d24ffc
andreeagulie <andreea_gulie95@yahoo.com>
2017-01-17 22:58:31 +0200

    Add model Destination

--- Backpack/Proiect/models.py
+++ Backpack/Proiect/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Destination (models.Model):
+    Destination_name = models.CharField(max_length=200)
+    Country = models.CharField(max_length=200)
+    City = models.CharField(max_length=200)
+
 # Create your models here.
