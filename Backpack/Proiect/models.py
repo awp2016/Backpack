facf01beb7e48397f017944e2f1a82c881878b59
cosminalexandru <silvian1995@yahoo.com>
2017-01-17 22:56:17 +0200

    Add model Review

--- Backpack/Proiect/models.py
+++ Backpack/Proiect/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Status(models.Model):
+	Text = models.CharField(max_length=100)
+	Date_added = models.DateTimeField(auto_now_add=True)
+	Avatar = models.ImageField(null=True, upload_to='images')
+	Rating = models.IntegerField()
+	User_id = models.ForeignKey(User,on_delete=models.CASCADE)
+	Destination_id = models.ForeignKey(Destination,on_delete=models.CASCADE)
+
+
+
