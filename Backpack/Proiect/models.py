68bc5c2cba6d874ab49f2a09cdc94c8c62abe4c2
cosminalexandru <silvian1995@yahoo.com>
2017-01-18 01:09:41 +0200

    Add last mode

--- Backpack/Proiect/models.py
+++ Backpack/Proiect/models.py
@@ -30,3 +30,10 @@ class Wishlist(models.Model):
 	Destination_id = models.ForeignKey(Destination)
 	User_id = models.ForeignKey(User)
 	
+class Review(models.Model):
+	Text = models.CharField(max_length=100)
+	Date_added = models.DateTimeField(auto_now_add=True)
+	Avatar = models.ImageField(null=True, upload_to='images')
+	Rating = models.IntegerField()
+	User_id = models.ForeignKey(User,on_delete=models.CASCADE)
+	Destination_id = models.ForeignKey(Destination,on_delete=models.CASCADE)
