4e69694bbd78d2baa22207793b606decf50a8779
horatiumacarau <horatiumacarau@gmail.com>
2017-01-17 22:54:09 +0200

    add model Comment and Wishlist

--- Backpack/Proiect/models.py
+++ Backpack/Proiect/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Comment(models.Model):
+	Text = models.CharField(max_length=200)
+    Date_added = models.DateTimeField(auto_now_add=True)
+    Author = models.ForeignKey(User)
+    
+    
+class Wishlist(models.Model)
+	Destination_id = models.ForeignKey(Destination)
+	User_id = models.ForeignKey(User)
