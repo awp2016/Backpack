759d711324a0e914dc3de89277f98fdfd69c8c5d
horatiumacarau <horatiumacarau@gmail.com>
2017-01-18 01:02:53 +0200

    add models Comment and Wishlist

--- Backpack/Proiect/models.py
+++ Backpack/Proiect/models.py
@@ -14,9 +14,19 @@ class UserProfile(models.Model):
     Birthdate = models.DateField(null=True)
     Sex = models.CharField(max_length=1, choices=SEX_CHOICES, default=FEMALE)
     Avatar = models.ImageField(null=True, upload_to='Avatars')
-    #User = models.OneToOneField(User, primary_key=True, related_name='profile', on_delete = models.CASCADE)
+    User = models.OneToOneField(User, primary_key=True, related_name='profile', on_delete = models.CASCADE)
 	
 class Destination (models.Model):
     Destination_name = models.CharField(max_length=50)
     Country = models.CharField(max_length=50)
     City = models.CharField(max_length=50)
+
+class Comment(models.Model):
+	Text = models.CharField(max_length=200)
+	Date_added = models.DateTimeField(auto_now_add=True)
+	Author = models.ForeignKey(User)
+    
+class Wishlist(models.Model):
+	Destination_id = models.ForeignKey(Destination)
+	User_id = models.ForeignKey(User)
+	
