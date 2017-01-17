7cde0c7c75259b072dc135258e1323d732448677
cosminalexandru <silvian1995@yahoo.com>
2017-01-17 23:44:34 +0200

    Add migrations

--- Backpack/Proiect/models.py
+++ Backpack/Proiect/models.py
@@ -2,13 +2,13 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
-class Status(models.Model):
+class Review(models.Model):
 	Text = models.CharField(max_length=100)
 	Date_added = models.DateTimeField(auto_now_add=True)
 	Avatar = models.ImageField(null=True, upload_to='images')
 	Rating = models.IntegerField()
-	User_id = models.ForeignKey(User,on_delete=models.CASCADE)
-	Destination_id = models.ForeignKey(Destination,on_delete=models.CASCADE)
+	#User_id = models.ForeignKey(User,on_delete=models.CASCADE)
+	#Destination_id = models.ForeignKey(Destination,on_delete=models.CASCADE)
 
 
 
