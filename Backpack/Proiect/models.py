7f714f19ffffe3e73a7d1341a759ac68ce4b342f
AnaG <gliga.anamaria31@gmail.com>
2017-01-17 22:53:46 +0200

    Add model UserProfile

--- Backpack/Proiect/models.py
+++ Backpack/Proiect/models.py
@@ -1,5 +1,19 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    SEX_CHOICES = (
+        (MALE, 'M'),
+        (FEMALE, 'F'),
+    )
+    First_name = models.CharField(max_length=100)
+    Last_name = models.CharField(max_length=100)
+    Birthdate = models.DateField(null=True)
+    Sex = models.CharField(max_length=1, choices=SEX_CHOICES, default=FEMALE)
+    Avatar = models.ImageField(null=True, upload_to='Avatars')
+    User = models.OneToOneField(User, primary_key=True, related_name='profile', on_delete = models.CASCADE)
