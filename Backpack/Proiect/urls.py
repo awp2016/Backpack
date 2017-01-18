78d537d0342a707ff2c1ed8a8295918b09f05ccb
cosminalexandru <silvian1995@yahoo.com>
2017-01-18 14:32:53 +0200

    Add template and URL

--- Backpack/Proiect/urls.py
+++ Backpack/Proiect/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
-
 from . import views
+
 
 urlpatterns = [
 	url(r'^login', views.login_view, name="login"),
 	url(r'^logout/$', views.logout_view, name="logout"),
 	url(r'^userProfile/(?P<pk>\d+)/edit/$', views.EditProfile.as_view(), name="editProfile"),
 	url(r'^Destinations/$', views.ShowDestinations.as_view(), name="destinations"),
+	url(r'^profile/(?P<pk>\d+)/$', views.ProfileView.as_view(), name='profile_info'),
 ]
