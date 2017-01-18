e096cf30307fe60e65b5dd4ed664025adc81c604
cosminalexandru <silvian1995@yahoo.com>
2017-01-18 02:06:26 +0200

    Add ProfileView

--- Backpack/Proiect/views.py
+++ Backpack/Proiect/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from . import models
 
-# Create your views here.
+
+
+class ProfileView(DetailView):
+
+    model = models.UserProfile
+    template_name = 'TBackpack/profile.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProfileView, self).get_context_data(**kwargs)
+        return context
+
