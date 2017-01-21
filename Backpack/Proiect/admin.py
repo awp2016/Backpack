91cccc35fc8731f829780a91f319c5486b730a45
AnaG <gliga.anamaria31@gmail.com>
2017-01-21 09:34:58 +0200

    Add models in admin

--- Backpack/Proiect/admin.py
+++ Backpack/Proiect/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
+
 # Register your models here.
-admin.site.register(models.Status)
+admin.site.register(models.Destination)
 admin.site.register(models.Comment)
 admin.site.register(models.UserProfile)
 admin.site.register(models.Review)
